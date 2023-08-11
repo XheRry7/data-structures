fc3850dba786dd790a42fe20b76a59a56f400b83
Shehryar <shehryarahmed1744@gmail.com>
2023-08-11 14:49:43 -0700

    stacks and queues theory

---
+++ Stack_AND_QUEUES/index.js
@@ -0,0 +1,23 @@
+/**** Stacks and Queues are linear data structures. Linear data structures allows 
+us to traverse sequentially which means one by one. In which one data element can be directly reached */
+
+/* Stacks : LIFO : One scenario in which we use stacks are browser history in which we go back and forth
+. Another is when we write a text and want to undo that. UNDO and REDO use stack.It is basically something
+ in which we place the last added thing in the memory so we can get that thing on the first.
+ 
+Complexity:
+Lookup: O(n)
+pop(): O(1)
+push: O(1)
+peek: view the top most plate : O(1)
+ 
+ */
+/* Queue: FIFO: Queues are used in many scenarios like when we want to buy movie tickets. Also in restaurant app for checking tables. Most common example is a printer.
+Complexities:
+lookup: O(n)
+enqueue(push): O(1)
+dequeue(pop): It takes the first person to pop :  O(1)
+peek: first person in the list : O(1)
+
+
+*/
